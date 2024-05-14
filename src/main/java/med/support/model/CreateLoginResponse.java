cb85dbb8125921229e7710ebd3a4fc36cf458cc5
SuperHuma <theferganes@gmail.com>
2024-05-14 17:25:07 +0500

    commit-5

---
+++ src/main/java/med/support/model/CreateLoginResponse.java
@@ -0,0 +1,12 @@
+package med.support.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateLoginResponse {
+    private Long id;
+}
