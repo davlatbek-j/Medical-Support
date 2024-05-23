e1bd0f167655f4da73dc935716073d5e9594b312
SuperHuma <theferganes@gmail.com>
2024-05-23 14:11:25 +0500

    commit-login-front

---
+++ src/main/java/med/support/controller/HomeController.java
@@ -0,0 +1,16 @@
+package med.support.controller;
+
+import med.support.model.SignIn;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String login(Model model) {
+        model.addAttribute("signIn", new SignIn());
+        return "index";
+    }
+
+}
